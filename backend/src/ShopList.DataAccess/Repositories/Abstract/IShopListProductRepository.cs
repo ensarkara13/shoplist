091197d4f4e257cdbc491c245a3799bcc2c73bae
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-15 18:02:30 +0300

    shoplistproduct repo interface added

---
+++ backend/src/ShopList.DataAccess/Repositories/Abstract/IShopListProductRepository.cs
@@ -0,0 +1,6 @@
+using ShopList.Entities.Concrete;
+
+namespace ShopList.DataAccess.Repositories.Abstract
+{
+  public interface IShopListProductRepository : IGenericRepository<ShopListProduct> { }
+}
