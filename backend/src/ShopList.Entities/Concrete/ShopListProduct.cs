ac9314df8f3ca7eca08881dcae527e2750fac50e
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-16 23:07:34 +0300

    createdAt and modifiedAt properties added

--- backend/src/ShopList.Entities/Concrete/ShopListProduct.cs
+++ backend/src/ShopList.Entities/Concrete/ShopListProduct.cs
@@ -1,10 +1,14 @@
+using System;
 using ShopList.Entities.Abstract;
+
 
 namespace ShopList.Entities.Concrete
 {
   public class ShopListProduct
   {
     public string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime ModifiedAt { get; set; }
 
     public int ShopListId { get; set; }
     public ShoppingList ShoppingList { get; set; }
