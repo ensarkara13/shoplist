ae16fdb0d3cb101533034295ee8f27ab9b4a5187
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-17 23:07:31 +0300

    product manager implemented

--- backend/src/ShopList.Business/Concrete/ProductManager.cs
+++ backend/src/ShopList.Business/Concrete/ProductManager.cs
@@ -1,38 +1,105 @@
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using System.Threading.Tasks;
 using ShopList.Business.Abstract;
 using ShopList.Core.Utilities.Results;
+using ShopList.DataAccess.Repositories.Abstract;
 using ShopList.Entities.DTOs.Product;
+using AutoMapper;
+using FluentValidation;
+using FluentValidation.Results;
+using ShopList.Core.Extensions;
+using ShopList.Entities.Concrete;
 
 namespace ShopList.Business.Concrete
 {
   public class ProductManager : IProductService
   {
-    public Task<Result> AddProduct(ProductAddDto product)
+    private readonly IProductRepository _productRepository;
+    private readonly IValidator<ProductAddDto> _productAddValidator;
+    private readonly IValidator<ProductUpdateDto> _productUpdateValidator;
+    private readonly IMapper _mapper;
+    public ProductManager(IProductRepository productRepository, IValidator<ProductAddDto> productAddValidator, IValidator<ProductUpdateDto> productUpdateValidator, IMapper mapper)
     {
-      throw new NotImplementedException();
+      _productRepository = productRepository;
+      _productAddValidator = productAddValidator;
+      _productUpdateValidator = productUpdateValidator;
+      _mapper = mapper;
     }
 
-    public Task<Result> DeleteProduct(int id)
+    public async Task<Result> AddProduct(ProductAddDto productDto)
     {
-      throw new NotImplementedException();
+      ValidationResult validationResult = _productAddValidator.Validate(productDto);
+      if (validationResult.IsValid)
+      {
+        Product product = await _productRepository.Get(p => p.Name == productDto.Name);
+        if (product != null)
+        {
+          return Result.Failure("Ürün zaten mevcut");
+        }
+
+        product = _mapper.Map<Product>(productDto);
+        await _productRepository.Add(product);
+
+        return Result.Success("Ürün ekleme işlemi başarılı.");
+      }
+
+      return Result.Failure(validationResult.ConvertToCustomErrors());
     }
 
-    public Task<DataResult<ProductGetDto>> GetProduct(Expression<Func<ProductGetDto, bool>> filter)
+    public async Task<Result> DeleteProduct(int id)
     {
-      throw new NotImplementedException();
+      Product product = await _productRepository.Get(p => p.Id == id);
+      if (product == null)
+      {
+        return Result.Failure("Silinecek ürün bulunamadı.");
+      }
+
+      await _productRepository.Delete(product);
+      return Result.Success("Ürün başarı ile silindi.");
     }
 
-    public Task<DataResult<List<ProductGetDto>>> GetProductList(Expression<Func<ProductGetDto, bool>> filter = null)
+    public async Task<DataResult<ProductGetDto>> GetProductById(int id)
     {
-      throw new NotImplementedException();
+      Product product = await _productRepository.Get(p => p.Id == id);
+      if (product == null)
+      {
+        return DataResult<ProductGetDto>.Failure("İstenilen ürün bulunamadı.");
+      }
+
+      ProductGetDto productGetDto = _mapper.Map<ProductGetDto>(product);
+
+      return DataResult<ProductGetDto>.Success(productGetDto);
     }
 
-    public Task<Result> UpdateProduct(ProductUpdateDto product)
+    public async Task<DataResult<List<ProductGetDto>>> GetProductList()
     {
-      throw new NotImplementedException();
+      List<Product> productList = await _productRepository.GetAll();
+      if (productList == null)
+      {
+        return DataResult<List<ProductGetDto>>.Failure("Hiç ürün bulunmamaktadır.");
+      }
+
+      List<ProductGetDto> productGetDtos = _mapper.Map<List<ProductGetDto>>(productList);
+      return DataResult<List<ProductGetDto>>.Success(productGetDtos);
+    }
+
+    public async Task<Result> UpdateProduct(int id, ProductUpdateDto productDto)
+    {
+      Product product = await _productRepository.Get(p => p.Name == productDto.Name);
+      if (product != null)
+      {
+        return Result.Failure("Aynı isimde birden fazla ürün olamaz");
+      }
+      product = await _productRepository.Get(p => p.Id == id);
+      if (product == null)
+      {
+        return Result.Failure("Güncellenecek ürün bulunamadı.");
+      }
+
+      product = _mapper.Map<ProductUpdateDto, Product>(productDto, product);
+
+      return Result.Success("Güncelleme işlemi başarılı.");
     }
   }
 }
