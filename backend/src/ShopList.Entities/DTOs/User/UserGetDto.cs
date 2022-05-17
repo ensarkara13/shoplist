6f09874cc23a9d6bc3163b3f87ea30d690affb02
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-17 14:23:24 +0300

    necessary properties added

--- backend/src/ShopList.Entities/DTOs/User/UserGetDto.cs
+++ backend/src/ShopList.Entities/DTOs/User/UserGetDto.cs
@@ -5,5 +5,7 @@ namespace ShopList.Entities.DTOs.User
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
+    public string Role { get; set; }
+    public string Password { get; set; }
   }
 }
