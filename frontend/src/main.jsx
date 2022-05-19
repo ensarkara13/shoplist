828a680d9018db6e197c1a47491c5d482481f76e
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-19 23:12:13 +0300

    providers added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { AuthProvider } from "./contexts/authContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
