a4310f6f4b178a6f17122c70506d3873a00bf6a4
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-20 23:56:31 +0300

    login and register implementations

--- frontend/src/contexts/authContext.jsx
+++ frontend/src/contexts/authContext.jsx
@@ -1,14 +1,67 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { loginBackend, registerBackend } from "../../apiConnection";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isloggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
+  const [userId, setUserId] = useState();
+
+  useEffect(() => {
+    localStorage.setItem("chakra-ui-color-mode", "dark");
+  }, []);
+  useEffect(() => {
+    const id = JSON.parse(localStorage.getItem("user-id"));
+    if (id > 0) {
+      setIsLoggedIn(true);
+      setUserId(id);
+
+      const role = localStorage.getItem("user-role");
+      setIsAdmin(role === "Admin" ? true : false);
+    }
+  });
+
+  const register = async ({ firstName, lastName, email, password }) => {
+    const user = { firstName, lastName, email, password };
+    await registerBackend(user);
+  };
+
+  const login = async (user) => {
+    const { id, role, accessToken, message, errorMessages } =
+      await loginBackend(user);
+
+    if (message != null || errorMessages != null) {
+      console.log(`message: ${message}`);
+      console.log(`error messages: ${errorMessages}`);
+    }
+
+    localStorage.setItem("access-token", accessToken);
+    localStorage.setItem("user-role", role);
+    localStorage.setItem("user-id", id);
+
+    setIsLoggedIn(true);
+    setUserId(id);
+    setIsAdmin(role === "Admin" ? true : false);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("access-token");
+    localStorage.removeItem("user-role");
+    localStorage.removeItem("user-id");
+
+    setIsLoggedIn(false);
+    setIsAdmin(false);
+    setUserId(null);
+  };
 
   const values = {
-    isloggedIn,
+    isLoggedIn,
     isAdmin,
+    register,
+    login,
+    logout,
+    userId,
   };
 
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
