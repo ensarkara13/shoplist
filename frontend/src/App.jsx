ebbf91b38a389184cc738b10a875521cbe450639
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-20 23:57:00 +0300

    main routes defined

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,47 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import DashboardPage from "./pages/DashboardPage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import RegisterPage from "./pages/RegisterPage";
+import AnonymousRoute from "./routes/AnonymousRoute";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={
+              <AnonymousRoute>
+                <LoginPage />
+              </AnonymousRoute>
+            }
+          />
+          <Route
+            path="register"
+            element={
+              <AnonymousRoute>
+                <RegisterPage />
+              </AnonymousRoute>
+            }
+          />
+          <Route
+            path="dashboard"
+            element={
+              <ProtectedRoute>
+                <DashboardPage />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
