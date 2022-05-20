cc6395b4aec93cf0cda44d9931025f73d1993c0b
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-20 23:53:10 +0300

    login and register functions written

--- frontend/apiConnection.js
+++ frontend/apiConnection.js
@@ -1,18 +1,3 @@
-import axios from "axios";
-
-axios.interceptors.request.use(function (config) {
-  const { origin } = new URL(config.url);
-
-  const allowedOrigins = [import.meta.env.VITE_BASE_ENDPOINT];
-  const accessToken = document.cookie;
-
-  if (allowedOrigins.includes(origin)) {
-    config.headers.authorization = accessToken;
-  }
-
-  return config;
-});
-
 const baseEndpoint = `${import.meta.env.VITE_BASE_ENDPOINT}`;
 const authEndpoint = `${baseEndpoint}/auth`;
 const categoryEndpoint = `${baseEndpoint}/categories`;
@@ -20,5 +5,35 @@ const productEndpoint = `${baseEndpoint}/products`;
 const shopListEndpoint = `${baseEndpoint}/shoplists`;
 const shopListProductEndpoint = `${baseEndpoint}/shoplistproducts`;
 
-// AUTH
+const postRequestOptions = (value, isAuthenticated = false) => {
+  const config = {
+    method: "post",
+    body: JSON.stringify(value),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
+  if (isAuthenticated) {
+    config.headers["Authorization"] = `Bearer ${localStorage.getItem(
+      "access-token"
+    )}`;
+  }
+
+  return config;
+};
+// AUTH
+export const registerBackend = async (user) => {
+  const data = await fetch(
+    `${authEndpoint}/register`,
+    postRequestOptions(user)
+  );
+  const response = data.json();
+  return response;
+};
+
+export const loginBackend = async (user) => {
+  const data = await fetch(`${authEndpoint}/login`, postRequestOptions(user));
+  const response = data.json();
+  return response;
+};
