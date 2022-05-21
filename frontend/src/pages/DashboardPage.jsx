aa87f90d2660b7dcd6e810f16de96681e4c1b802
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-21 15:16:39 +0300

    page structured

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,6 +1,45 @@
+import { Box, Button, Flex, Text, Heading, Grid } from "@chakra-ui/react";
+import { useAuthContext } from "../contexts/authContext";
 
 function DashboardPage() {
-  return <div>DashboardPage</div>;
+  const { isAdmin } = useAuthContext();
+  return (
+    <>
+      {isAdmin && (
+        <Box m={2} p={2}>
+          <Button colorScheme={"green"} mx={1}>
+            Kategori Ekle
+          </Button>
+          <Button colorScheme={"green"} mx={1}>
+            Ürün Ekle
+          </Button>
+        </Box>
+      )}
+
+      <Flex align={"center"}>
+        <Box m={5}>
+          <Heading as={"h3"} size={"lg"} my={"4"}>
+            Kategoriler
+          </Heading>
+          {/* {categories?.length > 0 &&
+            categories.map((category, index) => {
+              return <CategoryBar key={index} category={category} />;
+            })} */}
+        </Box>
+        <Box m={5}>
+          <Heading as={"h3"} size={"lg"} m={"3"}>
+            Ürünler
+          </Heading>
+          <Grid templateColumns={"repeat(4, 1fr)"} gap={4}>
+            {/* {products?.length > 0 &&
+              products.map((product, index) => {
+                return <ProductBox key={index} product={product} />;
+              })} */}
+          </Grid>
+        </Box>
+      </Flex>
+    </>
+  );
 }
 
 export default DashboardPage;
