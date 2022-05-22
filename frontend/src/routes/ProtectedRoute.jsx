53c06332ba8c1e96b34be1cb47097a6a87c986ba
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-22 23:06:18 +0300

    minor changes

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,13 +1,14 @@
 import { Navigate } from "react-router-dom";
 import { useAuthContext } from "../contexts/authContext";
+import { ShopListProvider } from "../contexts/shopListContext";
 
 function ProtectedRoute({ children }) {
   const { isLoggedIn } = useAuthContext();
 
-  if (isLoggedIn) {
-    return children;
+  if (!isLoggedIn) {
+    return <Navigate to={"/"} />;
   }
-  return <Navigate to={"/"} />;
+  return <ShopListProvider>{children}</ShopListProvider>;
 }
 
 export default ProtectedRoute;
