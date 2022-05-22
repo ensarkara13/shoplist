912a2d3f34a7c040c0d085dfccd411ed34887f23
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-22 23:05:03 +0300

    getCategories function created

--- frontend/src/contexts/shopListContext.jsx
+++ frontend/src/contexts/shopListContext.jsx
@@ -1,9 +1,36 @@
-import { createContext, useContext } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { getCategoriesBackend } from "../../apiConnection";
 
 const ShopListContext = createContext();
 
 export const ShopListProvider = ({ children }) => {
-  const values = {};
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const getCategories = useMemo(() => {
+    return async () => {
+      const categoryResponse = await getCategoriesBackend();
+      setCategories(categoryResponse);
+    };
+  }, [categories]);
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  const values = {
+    categories,
+    products,
+    setProducts,
+    setCategories,
+  };
 
   return (
     <ShopListContext.Provider value={values}>
