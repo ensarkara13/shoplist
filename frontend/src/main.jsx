32d958540803e0cf37919f232c1883a8daedf39b
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-22 23:07:08 +0300

    shoplist provider removed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,15 +4,12 @@ import App from "./App";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AuthProvider } from "./contexts/authContext";
-import { ShopListProvider } from "./contexts/shopListContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
       <AuthProvider>
-        <ShopListProvider>
-          <App />
-        </ShopListProvider>
+        <App />
       </AuthProvider>
     </ChakraProvider>
   </React.StrictMode>
