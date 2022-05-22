05bccdf79b2ea2f49075e34007b3e832fa52da54
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-22 12:48:02 +0300

    shoplist context provider added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AuthProvider } from "./contexts/authContext";
+import { ShopListProvider } from "./contexts/shopListContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
       <AuthProvider>
-        <App />
+        <ShopListProvider>
+          <App />
+        </ShopListProvider>
       </AuthProvider>
     </ChakraProvider>
   </React.StrictMode>
