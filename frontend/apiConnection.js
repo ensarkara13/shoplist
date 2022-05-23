e10271e12b65e85afba97b5d5c9e864f27a44f43
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-23 23:50:37 +0300

    connections updated

--- frontend/apiConnection.js
+++ frontend/apiConnection.js
@@ -1,4 +1,10 @@
-import { get, post } from "./scripts/requests/index";
+import { requestOptions } from "./scripts/requestOptions";
+import {
+  getRequest,
+  postRequest,
+  putRequest,
+  deleteRequest,
+} from "./scripts/requests/index";
 
 const baseEndpoint = `${import.meta.env.VITE_BASE_ENDPOINT}`;
 const authEndpoint = `${baseEndpoint}/auth`;
@@ -9,30 +15,67 @@ const shopListProductEndpoint = `${baseEndpoint}/shoplistproducts`;
 
 // AUTH
 export const registerBackend = async (user) => {
-  return await post(`${authEndpoint}/register`, user, false);
+  // return await postRequest(`${authEndpoint}/register`, user, false);
+  const response = await fetch(
+    `${authEndpoint}/register`,
+    requestOptions({ method: "post", value: user })
+  );
+  return await response.json();
 };
 
 export const loginBackend = async (user) => {
-  return await post(`${authEndpoint}/login`, user, false);
+  // return await postRequest(`${authEndpoint}/login`, user, false);
+  const response = await fetch(
+    `${authEndpoint}/login`,
+    requestOptions({ method: "post", value: user })
+  );
+  return await response.json();
 };
 
 // GET
 export const getCategoriesBackend = async () => {
-  return await get(categoryEndpoint);
+  return await getRequest(categoryEndpoint);
 };
 
 export const getProductsBackend = async () => {
-  return await get(productEndpoint);
+  return await getRequest(productEndpoint);
 };
 
 export const getProductsByCategoryBackend = async (categoryId) => {
-  return await get(`${productEndpoint}?category=${categoryId}`);
+  return await getRequest(`${productEndpoint}?category=${categoryId}`);
 };
 
 export const getShopListsBackend = async () => {
-  return await get(shopListEndpoint);
+  return await getRequest(shopListEndpoint);
 };
 
 export const getShopListProductsBackend = async (userId) => {
-  return await get(`${shopListProductEndpoint}/${userId}`);
+  return await getRequest(`${shopListProductEndpoint}/${userId}`);
+};
+
+// ADD
+export const addCategoryBackend = async (category) => {
+  await postRequest(categoryEndpoint, category);
+};
+
+export const addProductBackend = async (product) => {
+  await postRequest(productEndpoint, product);
+};
+
+export const addShopListBackend = async (shopList) => {
+  await postRequest(shopListEndpoint, shopList);
+};
+
+export const addShopListProductBackend = async (shopListProduct) => {
+  await postRequest(shopListProductEndpoint, shopListProduct);
+};
+
+// UPDATE
+export const updateCategoryBackend = async (categoryId, category) => {
+  await putRequest(`${categoryEndpoint}/${categoryId}`, category);
+};
+
+// DELETE
+export const deleteCategoryBackend = async (categoryId) => {
+  await deleteRequest(`${categoryEndpoint}/${categoryId}`);
 };
