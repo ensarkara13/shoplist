846389b777d8983814864dd0f64f640da16fa664
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-23 23:52:14 +0300

    react query added

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,20 +1,47 @@
-import { Box, Button, Flex, Text, Heading, Grid } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Text,
+  Heading,
+  Grid,
+  Spinner,
+} from "@chakra-ui/react";
 import { useAuthContext } from "../contexts/authContext";
 import CategoryBar from "../components/CategoryBar";
+import ProductBox from "../components/ProductBox";
+import { useShopListContext } from "../contexts/shopListContext";
+import AddCategoryModal from "../components/AddCategoryModal";
+import AddProductModal from "../components/AddProductModal";
+import { useQuery } from "react-query";
+import { getCategoriesBackend, getProductsBackend } from "../../apiConnection";
+import { useEffect } from "react";
 
 function DashboardPage() {
   const { isAdmin } = useAuthContext();
+  const { setCategories, setProducts } = useShopListContext();
+  const { data: categoryData, isLoading: isCategoryLoading } = useQuery(
+    "admin:categories",
+    getCategoriesBackend
+  );
+  const { data: productData, isLoading: isProductLoading } = useQuery(
+    "admin:products",
+    getProductsBackend
+  );
+
+  useEffect(() => {
+    setCategories(categoryData);
+  }, [categoryData]);
+
+  console.log(categoryData);
+  console.log(productData);
 
   return (
     <>
       {isAdmin && (
         <Box m={2} p={2}>
-          <Button colorScheme={"green"} mx={1}>
-            Kategori Ekle
-          </Button>
-          <Button colorScheme={"green"} mx={1}>
-            Ürün Ekle
-          </Button>
+          <AddCategoryModal />
+          <AddProductModal />
         </Box>
       )}
 
@@ -23,20 +50,30 @@ function DashboardPage() {
           <Heading as={"h3"} size={"lg"} my={"4"}>
             Kategoriler
           </Heading>
-          {categories?.length > 0 &&
-            categories.map((category, index) => {
+          {isCategoryLoading && (
+            <Flex align={"center"} m={"5"}>
+              <Spinner />
+            </Flex>
+          )}
+          {categoryData?.length > 0 &&
+            categoryData.map((category, index) => {
               return <CategoryBar key={index} category={category} />;
             })}
         </Box>
+        {isProductLoading && (
+          <Flex align={"center"} m={"5"}>
+            <Spinner />
+          </Flex>
+        )}
         <Box m={5}>
           <Heading as={"h3"} size={"lg"} m={"3"}>
             Ürünler
           </Heading>
           <Grid templateColumns={"repeat(4, 1fr)"} gap={4}>
-            {/* {products?.length > 0 &&
-              products.map((product, index) => {
+            {productData?.length > 0 &&
+              productData.map((product, index) => {
                 return <ProductBox key={index} product={product} />;
-              })} */}
+              })}
           </Grid>
         </Box>
       </Flex>
