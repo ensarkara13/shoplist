6c30b43aee95ca42e73c3af1104cff53af4a55a0
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-23 17:17:14 +0300

    react query added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,25 @@ import App from "./App";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AuthProvider } from "./contexts/authContext";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
