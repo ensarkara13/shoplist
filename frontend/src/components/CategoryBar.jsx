9fb2f3f264f0c0e622eb13d1eeba995ce7e4e290
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-24 22:42:51 +0300

    style changes

--- frontend/src/components/CategoryBar.jsx
+++ frontend/src/components/CategoryBar.jsx
@@ -4,6 +4,12 @@ import {
   AccordionButton,
   Box,
   Button,
+  Flex,
+  SimpleGrid,
+  Tag,
+  TagLabel,
+  TagCloseButton,
+  TagRightIcon,
 } from "@chakra-ui/react";
 import { useQueryClient, useMutation } from "react-query";
 import { deleteCategoryBackend } from "../../apiConnection";
@@ -16,39 +22,72 @@ function CategoryBar({ category }) {
 
   const handleDelete = (id) => {
     if (confirm("Silmek istediğinize emin misiniz?")) {
-      deleteMutation.mutate(id, {
-        onSuccess: () => console.log("Success"),
-      });
+      deleteMutation.mutate(id);
     }
   };
 
   return (
-    <Accordion>
-      <AccordionItem backgroundColor={"green.500"}>
-        <h2>
-          <AccordionButton>
-            <Box
-              flex="1"
-              textAlign="left"
-              // onClick={() => getProducts(category.id)}
-            >
-              {category.name}
-            </Box>
-            <Box
-              ms={"2"}
-              backgroundColor={"red.300"}
-              p={"2"}
+    <>
+      <Box
+        backgroundColor={"blue.800"}
+        borderRadius={"10px"}
+        textAlign={"center"}
+        my={"3"}
+        p={"2"}
+      >
+        <Box
+          width={"100%"}
+          backgroundColor={"blue.400"}
+          borderRadius={"5px"}
+          height="30px"
+        >
+          {category.name}
+        </Box>
+        <SimpleGrid columns={2} spacing={5} m={"2"}>
+          <Box>
+            <Tag
+              colorScheme={"red"}
+              as="button"
               onClick={() => handleDelete(category.id)}
             >
-              Sil
-            </Box>
-            <Box backgroundColor={"blue.300"} p={"2"} ms={"2"}>
-              Düzenle
-            </Box>
-          </AccordionButton>
-        </h2>
-      </AccordionItem>
-    </Accordion>
+              <TagLabel>Sil</TagLabel>
+            </Tag>
+          </Box>
+          <Box>
+            <Tag colorScheme={""} as="button">
+              <TagLabel>Düzenle</TagLabel>
+            </Tag>
+          </Box>
+        </SimpleGrid>
+      </Box>
+    </>
+
+    // <Box textAlign={"center"} m={"3"}>
+    //   <Box backgroundColor={"green.500"} p={"3"}>
+    //     <h2>
+    //       <Box>
+    //         <Box
+    //           flex="1"
+    //           textAlign="left"
+    //           // onClick={() => getProducts(category.id)}
+    //         >
+    //           {category.name}
+    //         </Box>
+    //         <Box
+    //           ms={"2"}
+    //           backgroundColor={"red.300"}
+    //           p={"2"}
+    //           onClick={() => handleDelete(category.id)}
+    //         >
+    //           Sil
+    //         </Box>
+    //         <Box backgroundColor={"blue.300"} p={"2"} ms={"2"}>
+    //           Düzenle
+    //         </Box>
+    //       </Box>
+    //     </h2>
+    //   </Box>
+    // </Box>
   );
 }
 
