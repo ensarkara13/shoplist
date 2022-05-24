ba101abe1504eb30d4c420bea796e9408d7a2616
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-24 16:56:27 +0300

    delete function added

--- frontend/src/components/ProductBox.jsx
+++ frontend/src/components/ProductBox.jsx
@@ -1,15 +1,21 @@
 import { Box, Button } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../contexts/authContext";
+import { useMutation, useQueryClient } from "react-query";
+import { deleteProductBackend } from "../../apiConnection";
 
 function ProductBox({ product }) {
   const { isAdmin } = useAuthContext();
+  const queryClient = useQueryClient();
+  const deleteMutation = useMutation(deleteProductBackend, {
+    onSuccess: () => queryClient.invalidateQueries("admin:products"),
+  });
 
-  // const handleDelete = async (id) => {
-  //   if (confirm("Silmek istediğinize emin misiniz?")) {
-  //     navigate("/home");
-  //   }
-  // };
+  const handleDelete = async (id) => {
+    if (confirm("Silmek istediğinize emin misiniz?")) {
+      deleteMutation.mutate(id);
+    }
+  };
 
   return (
     <Box m={3} backgroundColor={"green.500"} borderRadius={5} p={5}>
@@ -22,14 +28,14 @@ function ProductBox({ product }) {
           <Button
             colorScheme={"red"}
             mx={1}
-            // onClick={() => handleDelete(product.id)}
+            onClick={() => handleDelete(product.id)}
           >
             Sil
           </Button>
         </>
       )}
       {!isAdmin && (
-        <Button color={"blue.400"} mx={1}>
+        <Button colorScheme={"blue"} mx={1}>
           Ekle
         </Button>
       )}
