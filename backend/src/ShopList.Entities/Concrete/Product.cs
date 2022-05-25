e7994f3b1cfda0d7ac79e6b5fdfe9dad2fcb6b6b
Ensar Karamürteza <ensarkara13@gmail.com>
2022-05-25 23:06:43 +0300

    CreatedBy and ModifiedBy properties removed

--- backend/src/ShopList.Entities/Concrete/Product.cs
+++ backend/src/ShopList.Entities/Concrete/Product.cs
@@ -6,9 +6,7 @@ namespace ShopList.Entities.Concrete
   public class Product : EntityBase
   {
     public string Name { get; set; }
-    public string CreatedBy { get; set; }
-    public string ModifiedBy { get; set; }
-
+    
     public int CategoryId { get; set; }
     public Category Category { get; set; }
     public ICollection<ShopListProduct> ShopListProducts { get; set; }
