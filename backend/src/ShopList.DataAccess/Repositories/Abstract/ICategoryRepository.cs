eb9b843f4657f0ecbe447e7647e2b77abf480db3
Ensar Karamürteza <ensarkara13@gmail.com>
2022-06-15 15:34:24 +0300

    Added GetCategoriesWithProduct method signature

--- backend/src/ShopList.DataAccess/Repositories/Abstract/ICategoryRepository.cs
+++ backend/src/ShopList.DataAccess/Repositories/Abstract/ICategoryRepository.cs
@@ -1,8 +1,12 @@
 using ShopList.Entities.Concrete;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 
 namespace ShopList.DataAccess.Repositories.Abstract
 {
   public interface ICategoryRepository : IGenericRepository<Category>
   {
+    Task<List<Category>> GetCategoriesWithProducts();
   }
 }
