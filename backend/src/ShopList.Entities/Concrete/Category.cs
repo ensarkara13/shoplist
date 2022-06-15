962ca3652a544aece11812902703e8a8759d41a5
Ensar Karamürteza <ensarkara13@gmail.com>
2022-06-15 15:30:40 +0300

    Initialized nav property

--- backend/src/ShopList.Entities/Concrete/Category.cs
+++ backend/src/ShopList.Entities/Concrete/Category.cs
@@ -5,8 +5,12 @@ namespace ShopList.Entities.Concrete
 {
   public class Category : EntityBase
   {
+    public Category()
+    {
+      Products = new HashSet<Product>();
+    }
     public string Name { get; set; }
-    
+
     public ICollection<Product> Products { get; set; }
   }
 }
