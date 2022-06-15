0f0a4682363306bfec48eae5ae5ea052e449842a
Ensar Karamürteza <ensarkara13@gmail.com>
2022-06-15 15:35:09 +0300

    Implemented GetCategoriesWithProduct method

--- backend/src/ShopList.DataAccess/Repositories/Concrete/EntityFramework/EFCategoryRepository.cs
+++ backend/src/ShopList.DataAccess/Repositories/Concrete/EntityFramework/EFCategoryRepository.cs
@@ -1,13 +1,25 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using ShopList.DataAccess.Contexts;
 using ShopList.DataAccess.Repositories.Abstract;
 using ShopList.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
 
 namespace ShopList.DataAccess.Repositories.Concrete.EntityFramework
 {
   public class EFCategoryRepository : GenericRepository<Category>, ICategoryRepository
   {
+    private readonly ShopListDbContext _context;
     public EFCategoryRepository(ShopListDbContext context) : base(context)
     {
+      _context = context;
+    }
+
+    public async Task<List<Category>> GetCategoriesWithProducts()
+    {
+      return await _context.Categories.Include(c => c.Products).ToListAsync();
     }
   }
 }
